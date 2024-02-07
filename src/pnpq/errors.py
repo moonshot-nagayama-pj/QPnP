9870672cca5f05bae58632a5147fa4afd92518e8
zigen <hrlclb@gmail.com>
2024-02-07 11:46:01 +0900

    ✨ feat(errors.py): add custom exceptions for device disconnection and port not found errors to improve error handling and provide more specific error messages

---
+++ src/pnpq/errors.py
@@ -0,0 +1,7 @@
+class DeviceDisconnectedError(Exception):
+    """Exception raised for the device is disconnected"""
+    pass
+
+class DevicePortNotFoundError(Exception):
+    """Rasied when a port not found"""
+    pass
