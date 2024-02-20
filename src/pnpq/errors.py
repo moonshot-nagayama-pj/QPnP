63c1ea3c9106761f3f3c07ace2eb31a38ce410d9
Amin Taherkhani <amin@sfc.wide.ad.jp>
2024-02-20 20:04:47 +0900

    Error Handling Waveplate + Thorlab ODL

--- src/pnpq/errors.py
+++ src/pnpq/errors.py
@@ -16,6 +16,36 @@ class WaveplateInvalidStepsError(Exception):
     pass
 
 
+class WavePlateHomedNotCompleted(Exception):
+    """Raised when a Homed response has not been received from WavePlate Rotator device"""
+
+    pass
+
+
+class WavePlateCustomRotateError(Exception):
+    """Raised when custom rotation failed"""
+
+    pass
+
+
+class WavePlateMoveNotCompleted(Exception):
+    """Raised when Moved Complete response has not been receieved from WavePlate Rotator device"""
+
+    pass
+
+
+class WavePlateGetPosNotCompleted(Exception):
+    """Raised when GetPos response has not been received from Waveplate Rotator Device"""
+
+    pass
+
+
+class WaveplateEnableChannelError(Exception):
+    """Raised when no response has been received from Enable Channel Command"""
+
+    pass
+
+
 class WaveplateInvalidDegreeError(Exception):
     """Raised when an invalid degree specified. degree must be in a range 0-360"""
 
@@ -24,5 +54,29 @@ class WaveplateInvalidDegreeError(Exception):
 
 class WaveplateInvalidMotorChannelError(Exception):
     """Raised when trying to access an invalid motor channel number. check max_channel"""
+
+    pass
+
+
+class OdlMoveNotComepleted(Exception):
+    """Raised when Move complete response has not been received from ODL device"""
+
+    pass
+
+
+class OdlHomeNotCompleted(Exception):
+    """Raised when Homed response has not been received from ODL device"""
+
+    pass
+
+
+class OdlMoveOutofRangeError(Exception):
+    """Raised when the requesed move is our of range of the odl device"""
+
+    pass
+
+
+class OdlGetPosNotCompleted(Exception):
+    """Raised when no response has been received for GetPos command"""
 
     pass
