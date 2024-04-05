cfa4f81394ab4ce024c6ae7718c19bfa11fa9e76
Amin Taherkhani <amin@sfc.wide.ad.jp>
2024-04-05 19:12:25 +0900

    remove duplicate check_usb_hub_connected()

--- src/pnpq/utils.py
+++ src/pnpq/utils.py
@@ -14,18 +14,3 @@ def get_available_port(device_serial_number: str) -> str | None:
             logger.debug(f"port found: {port.device}")
             return port.device
     return None
-
-
-def check_usb_hub_connected() -> bool:
-    logger.debug(f"check_usb_hub_connected")
-
-    usb2_hub = usb.core.find(idVendor=0x2109, idProduct=0x2817)
-    usb3_hub = usb.core.find(idVendor=0x2109, idProduct=0x0817)
-
-    if usb2_hub is None and usb3_hub is None:
-        logger.debug(f"usb_hub is not detected")
-        return False
-
-    else:
-        logger.debug(f"usb_hub is detected")
-        return True
