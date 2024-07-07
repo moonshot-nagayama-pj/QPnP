59de22961cd06c91d509cfd9c1ef9342db31a63b
Tom Shen <40376288+TomShen1234@users.noreply.github.com>
2024-07-07 21:40:04 +0900

    Return types and docstrings

--- src/pnpq/devices/waveplate_stub.py
+++ src/pnpq/devices/waveplate_stub.py
@@ -15,6 +15,7 @@ from pnpq.errors import (
 )
 
 class Waveplate:
+    """Stub Waveplate Device Class"""
     resolution: int
     max_steps: int
     relative_home: float
@@ -60,16 +61,19 @@ class Waveplate:
     def __stub_check_channel(self, chanid: int):
         return chanid in self.enabled_channels
 
-    def connect(self):
+    def connect(self) -> None:
+        """Establish connection to the device"""
         self.connected = True
         self.logger.info("Stub Waveplate Connected")
 
-    def identify(self):
+    def identify(self) -> None:
+        """Identify the device by flashing the on-device LED"""
         self.__ensure_port_open()
         # Flashes LED on real device, for stub, we will do nothing
         self.logger.info("Stub Waveplate Identify")
 
-    def home(self):
+    def home(self) -> None:
+        """Perform homing operation on the device"""
         self.__ensure_port_open()
         self.logger.info("Stub Waveplate Home")
 
@@ -83,17 +87,20 @@ class Waveplate:
         time.sleep(1)
         self.logger.info("Home position: %s", self.current_position)
 
-    def auto_update_start(self):
+    def auto_update_start(self) -> None:
+        """Start automatically logging device information"""
         self.__ensure_port_open()
         self.logger.info("Stub Waveplate Auto Update Start")
         # TODO: Auto updating logging
 
-    def auto_update_stop(self):
+    def auto_update_stop(self) -> None:
+        """Stop automatically logging device information"""
         self.__ensure_port_open()
         self.logger.info("Stub Waveplate Auto Update Stop")
         # TODO: Auto updating logging
 
-    def disable_channel(self, chanid: int):
+    def disable_channel(self, chanid: int) -> None:
+        """Disable a channel on the device specified by chanid"""
         self.__ensure_port_open()
         self.logger.info("Stub Waveplate Disable Channel: %s", chanid)
 
@@ -106,7 +113,8 @@ class Waveplate:
         # Simulated delay
         time.sleep(0.1)
 
-    def enable_channel(self, chanid: int):
+    def enable_channel(self, chanid: int) -> None:
+        """Enable a channel on the device specified by chanid"""
         self.__ensure_port_open()
         self.logger.info("Stub Waveplate Enable Channel: %s", chanid)
         if chanid > self.max_channel:
@@ -120,15 +128,18 @@ class Waveplate:
         # TODO: Return a fake reply from the device
 
     def device_resolution(self) -> int:
+        """Get the device resolution"""
         return self.resolution
 
-    def getpos(self):
+    def getpos(self) -> int | float:
+        """Get the current position of the device"""
         self.__ensure_port_open()
         self.logger.info("Stub Waveplate Get Position")
         self.logger.info("Current Position: Steps: %s Degrees: %s", self.current_position, self.current_position / self.resolution)
         return self.current_position
 
-    def rotate(self, degree: int | float):
+    def rotate(self, degree: int | float) -> None:
+        """Rotate the device to a specified degree"""
         self.__ensure_port_open()
         self.__ensure_valid_degree(degree)
 
@@ -145,25 +156,29 @@ class Waveplate:
         time.sleep(abs(move_position - self.current_position) / 1000)
         # TODO: Return a fake reply from the device
 
-    def step_backward(self, steps: int):
+    def step_backward(self, steps: int) -> None:
+        """Step backward by a specified number of steps"""
         # Convert steps to degrees
         new_steps = self.current_position - steps
         degrees = new_steps / self.resolution
         self.rotate(-degrees)
 
-    def step_forward(self, steps: int):
+    def step_forward(self, steps: int) -> None:
+        """Step forward by a specified number of steps"""
         # Convert steps to degrees
         new_steps = self.current_position + steps
         degrees = new_steps / self.resolution
         self.rotate(degrees)
 
-    def rotate_relative(self, degree: int | float):
+    def rotate_relative(self, degree: int | float) -> None:
+        """Rotate the device by a specified degree relative to the current position"""
         # Get new rotation degree
         current_degree = self.current_position / self.resolution
         new_degree = current_degree + degree
         self.rotate(new_degree)
 
-    def custom_home(self, degree):
+    def custom_home(self, degree) -> None:
+        """Set custom home position for the device"""
         self.__ensure_port_open()
         self.__ensure_valid_degree(degree)
 
@@ -177,7 +192,8 @@ class Waveplate:
         self.relative_home = degree
         self.rotate(degree)
 
-    def custom_rotate(self, degree):
+    def custom_rotate(self, degree) -> None:
+        """Rotate the device to a specified degree relative to the custom home position"""
         self.__ensure_port_open()
         self.__ensure_valid_degree(degree)
 
