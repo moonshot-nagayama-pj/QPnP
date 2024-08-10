a8660197917c9c96dfd95a28e7a91344f5460f29
Tom Shen <40376288+TomShen1234@users.noreply.github.com>
2024-08-10 13:32:49 +0900

    Update worker's system version

--- bin/install-dependencies.bash
+++ bin/install-dependencies.bash
@@ -30,26 +30,6 @@ trap_exit() {
 }
 trap trap_exit EXIT
 
-# Update package list and install curl and shellcheck
+# Update package list and install shfmt (other dependencies are built-in)
 sudo apt-get update
-sudo apt-get install curl shellcheck -y
-
-# Install shfmt
-download_dir=$(mktemp -d 'shfmt.XXXXXXXX')
-exec_name="shfmt_v3.8.0_linux_amd64"
-exec_path="${download_dir}"/"${exec_name}"
-sha256_name="sha256sums.txt"
-sha256_path="${download_dir}"/"${sha256_name}"
-curl --location \
-  --output-dir "${download_dir}" \
-  --remote-name-all \
-  "https://github.com/mvdan/sh/releases/download/v3.8.0/${sha256_name}" \
-  "https://github.com/mvdan/sh/releases/download/v3.8.0/${exec_name}"
-# Get the first part of the shasum string for the correct file, which is formatted as "<checksum> <filename>"
-shasum_str=$(grep "${exec_name}" "${sha256_path}" | cut -d ' ' -f 1)
-# Write it back to the file for comparison
-stdmsg "${shasum_str}" >"${sha256_path}"
-# shellcheck disable=SC2312
-sha256sum --check <(stdmsg "$(<"${sha256_path}") ${exec_path}")
-mv "${exec_path}" /usr/local/bin/shfmt
-chmod +x /usr/local/bin/shfmt
+sudo apt-get install golang-mvdan-sh -y
