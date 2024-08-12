211baeb699a286497de306e775c64ff9c16862e0
Andrew Todd <at@auspicacious.org>
2024-08-12 17:17:24 +0900

    Fix formatting errors reported by black.

--- tests/switch_stub_test.py
+++ tests/switch_stub_test.py
@@ -1,8 +1,7 @@
 from pnpq.devices.switch_stub import Switch
-from pnpq.errors import (
-    DeviceDisconnectedError
-)
+from pnpq.errors import DeviceDisconnectedError
 import pytest
+
 
 @pytest.fixture
 def connected_switch():
@@ -10,15 +9,18 @@ def connected_switch():
     switch.connect()
     return switch
 
+
 @pytest.mark.parametrize("f", [("bar_state"), ("cross")])
 def test_access_without_connection(f):
-    switch = Switch() # noqa: F841
+    switch = Switch()  # noqa: F841
     with pytest.raises(DeviceDisconnectedError):
         eval(f"switch.{f}()")
+
 
 def test_set_bar_state(connected_switch):
     connected_switch.bar_state()
     assert connected_switch.state == 1
+
 
 def test_set_cross_state(connected_switch):
     connected_switch.cross()
