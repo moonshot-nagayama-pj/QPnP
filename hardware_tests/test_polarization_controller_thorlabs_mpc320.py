5bd846f588c0b5517aa375781e219a4ac39f8e60
Tom Shen <40376288+TomShen1234@users.noreply.github.com>
2024-10-28 22:39:25 +0900

    Updates hardware tests

--- hardware_tests/test_polarization_controller_thorlabs_mpc320.py
+++ hardware_tests/test_polarization_controller_thorlabs_mpc320.py
@@ -6,12 +6,13 @@ from pnpq.devices.polarization_controller_thorlabs_mpc320 import (
     PolarizationControllerThorlabsMPC320,
 )
 from pnpq.units import ureg
+from pnpq.apt.connection import AptConnection
 
 
 @pytest.fixture(name="device", scope="module")
 def device_fixture() -> PolarizationControllerThorlabsMPC320:
-    return PolarizationControllerThorlabsMPC320(serial_number="38454784")
-    # return PolarizationControllerThorlabsMPC320(serial_number="38444954")
+    connection = AptConnection(serial_number="38454784")
+    return PolarizationControllerThorlabsMPC320(connection=connection)
 
 
 # def test_check_status(device: PolarizationControllerThorlabsMPC320) -> None:
