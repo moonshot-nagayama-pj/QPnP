c33a693bb5fb7ad175255d3b59d0ee6bb2b3782c
larz <lm@keio.jp>
2024-11-14 23:18:47 +0900

    Update tests to parametrized tests (-ω-)/

--- tests/test_pint_units.py
+++ tests/test_pint_units.py
@@ -3,39 +3,57 @@ import pytest
 from pnpq.units import ureg
 
 
-def test_mpc320_step_conversion() -> None:
+@pytest.mark.parametrize(
+    "test_mpc320_step, expected_angle",
+    [
+        (-100, -100 * (170 / 1370)),
+        (0, 0),
+        (100, 100 * (170 / 1370)),
+    ],
+)
+def test_mpc320_step_to_angle_conversion(
+    test_mpc320_step: float, expected_angle: float
+) -> None:
 
-    for value in [-100, 0, 100]:  # Test few values
-
-        pint_degree = (value * ureg.mpc320_step).to("degrees").magnitude
-        decimal_degree = value * (170 / 1370)
-
-        assert pint_degree == pytest.approx(decimal_degree)
-
-        pint_mpc320_step = (value * ureg.degree).to("mpc320_step").magnitude
-        decimal_mpc_320_step = value / (170 / 1370)
-
-        assert pint_mpc320_step == pytest.approx(decimal_mpc_320_step)
+    angle = (test_mpc320_step * ureg.mpc320_step).to("degrees").magnitude
+    assert angle == pytest.approx(expected_angle)
 
 
-def test_mpc320_velocity_conversion() -> None:
+@pytest.mark.parametrize(
+    "test_angle, expected_mpc320_step",
+    [
+        (-100, -100 / (170 / 1370)),
+        (0, 0),
+        (100, 100 / (170 / 1370)),
+    ],
+)
+def test_angle_to_mpc320_step_conversion(
+    test_angle: float, expected_mpc320_step: float
+) -> None:
 
-    velocity = 200 * ureg("degree / second")
+    mpc320_step = (test_angle * ureg.degree).to("mpc320_step").magnitude
+    assert mpc320_step == pytest.approx(expected_mpc320_step)
+
+
+@pytest.mark.parametrize(
+    "deg_per_sec_velocity, mpc320_velocity",
+    [
+        (200, 50),
+        (300, 75),
+    ],
+)
+def test_mpc320_velocity_conversion(
+    deg_per_sec_velocity: float, mpc320_velocity: float
+) -> None:
+
+    # Test that degrees / second quantities accurately convert into mpc320_velocity quantities
+    velocity = deg_per_sec_velocity * ureg("degree / second")
     proportion = velocity.to("mpc320_velocity")
-    assert 50 == pytest.approx(proportion.magnitude)
+    assert mpc320_velocity == pytest.approx(proportion.magnitude)
     assert "mpc320_velocity" == proportion.units
 
-    proportion = 50 * ureg.mpc320_velocity
+    # Test that mpc320_velocity quantities accurately convert back into degrees / second quantities
+    proportion = mpc320_velocity * ureg.mpc320_velocity
     velocity = proportion.to("degree / second")
-    assert 200 == pytest.approx(velocity.magnitude)
-    assert "degree / second" == velocity.units
-
-    velocity = 300 * ureg("degree / second")
-    proportion = velocity.to("mpc320_velocity")
-    assert 75 == pytest.approx(proportion.magnitude)
-    assert "mpc320_velocity" == proportion.units
-
-    proportion = 75 * ureg.mpc320_velocity
-    velocity = proportion.to("degree / second")
-    assert 300 == pytest.approx(velocity.magnitude)
+    assert deg_per_sec_velocity == pytest.approx(velocity.magnitude)
     assert "degree / second" == velocity.units
