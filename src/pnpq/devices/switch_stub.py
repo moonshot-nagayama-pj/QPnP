6e28b87f0661aa34f8ef7d12c752010840333a71
Tom Shen <40376288+TomShen1234@users.noreply.github.com>
2024-11-15 16:51:26 +0900

    Updated logging for stub switch

--- src/pnpq/devices/switch_stub.py
+++ src/pnpq/devices/switch_stub.py
@@ -1,10 +1,14 @@
-import logging
+import structlog
+
+from ..events import Event
 
 from pnpq.errors import DeviceDisconnectedError
 
 
 class Switch:
     """Stub Switch Device Class"""
+
+    log = structlog.get_logger()
 
     def __init__(self) -> None:
         # Stub Serial Number
@@ -18,25 +22,24 @@ class Switch:
         # 1 is bar state, 2 is cross state. These are the numbers used to represent these states in the Thorlabs Optical Switch 1310E driver.
         self.state: int = 1
 
-        # Logger for this class
-        self.logger: logging.Logger = logging.getLogger(f"{self}")
-
     def __ensure_port_open(self) -> None:
         if not self.connected:
-            self.logger.error("Device not connected")
+            self.log.error(event=Event.DEVICE_NOT_CONNECTED_ERROR)
             raise DeviceDisconnectedError(f"{self} is disconnected")
 
     def connect(self) -> None:
         """Establish connection to the device"""
         self.connected = True
-        self.logger.info("Stub Waveplate Connected")
+        self.log.debug("Stub Waveplate Connected")
 
     def bar_state(self) -> None:
         """Sets the optical switch's state to 1 (bar state)"""
         self.__ensure_port_open()
         self.state = 1
+        self.log.info(event=Event.SWITCH_BAR_STATE)
 
     def cross(self) -> None:
         """Sets the optical switch's state to 2 (cross state)"""
         self.__ensure_port_open()
         self.state = 2
+        self.log.info(event=Event.SWITCH_CROSS_STATE)
--- src/pnpq/events.py
+++ src/pnpq/events.py
@@ -9,3 +9,11 @@ class Event(StrEnum):
     TX_MESSAGE_ORDERED = auto()
     TX_MESSAGE_UNORDERED = auto()
     UNCAUGHT_EXCEPTION = auto()
+
+    # General hardware errors
+    DEVICE_CONNECTED = auto()
+    DEVICE_NOT_CONNECTED_ERROR = auto()
+
+    # Optical Switch Events
+    SWITCH_BAR_STATE = auto()
+    SWITCH_CROSS_STATE = auto()
