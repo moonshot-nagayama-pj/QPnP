277242fde138b61cc8bb6ce372f2f3adb599d48c
larz <lm@keio.jp>
2024-11-26 23:36:13 +0900

    Update tests to be more simpler (*^-^*)

--- tests/test_pint_units.py
+++ tests/test_pint_units.py
@@ -1,5 +1,3 @@
-from math import pi
-
 import pytest
 from pint import Quantity
 
@@ -9,9 +7,9 @@ from pnpq.units import pnpq_ureg
 @pytest.mark.parametrize(
     "test_mpc320_step, expected_angle",
     [
-        (-100, -100 * (170 / 1370)),
+        (-1370, -170),
         (0, 0),
-        (100, 100 * (170 / 1370)),
+        (1370, 170),
     ],
 )
 def test_mpc320_step_to_angle_conversion(
@@ -25,9 +23,9 @@ def test_mpc320_step_to_angle_conversion(
 @pytest.mark.parametrize(
     "test_angle, expected_mpc320_step",
     [
-        (-100, -100 / (170 / 1370)),
+        (-170, -1370),
         (0, 0),
-        (100, 100 / (170 / 1370)),
+        (170, 1370),
     ],
 )
 def test_angle_to_mpc320_step_conversion(
@@ -40,58 +38,58 @@ def test_angle_to_mpc320_step_conversion(
 
 # Test that [angle] / second quantities accurately convert into mpc320_velocity quantities
 @pytest.mark.parametrize(
-    "angular_velocity, unit, mpc320_velocity",
+    "angular_velocity, mpc320_velocity",
     [
-        (200, pnpq_ureg("degree / second"), 50),
-        (300, pnpq_ureg("degree / second"), 75),
-        (201, pnpq_ureg("degree / second"), 50),
-        (299, pnpq_ureg("degree / second"), 75),
-        (200 * (pi / 180), pnpq_ureg("radian / second"), 50),
-        (300 * (pi / 180), pnpq_ureg("radian / second"), 75),
-        (201 * (pi / 180), pnpq_ureg("radian / second"), 50),
-        (299 * (pi / 180), pnpq_ureg("radian / second"), 75),
-        (200 / (170 / 1370), pnpq_ureg("mpc320_step / second"), 50),
-        (300 / (170 / 1370), pnpq_ureg("mpc320_step / second"), 75),
+        (200 * pnpq_ureg("degree / second"), 50),
+        (300 * pnpq_ureg("degree / second"), 75),
+        (201 * pnpq_ureg("degree / second"), 50),
+        (299 * pnpq_ureg("degree / second"), 75),
+        (3.49065850399 * pnpq_ureg("radian / second"), 50),
+        (5.23598775598 * pnpq_ureg("radian / second"), 75),
+        (3.50811179651 * pnpq_ureg("radian / second"), 50),
+        (5.21853446346 * pnpq_ureg("radian / second"), 75),
+        (1611.76470588 * pnpq_ureg("mpc320_step / second"), 50),
+        (2417.64705882 * pnpq_ureg("mpc320_step / second"), 75),
     ],
 )
 def test_to_mpc320_velocity_conversion(
-    angular_velocity: float, unit: Quantity, mpc320_velocity: float
+    angular_velocity: Quantity, mpc320_velocity: float
 ) -> None:
 
-    velocity = angular_velocity * unit
-    proportion = velocity.to("mpc320_velocity")
-    assert mpc320_velocity == pytest.approx(proportion.magnitude)
+    proportion = angular_velocity.to("mpc320_velocity")
+    assert mpc320_velocity == proportion.magnitude
     assert "mpc320_velocity" == proportion.units
 
 
 # Test that mpc320_velocity quantities accurately convert back into [angle] / second quantities
 @pytest.mark.parametrize(
-    "angular_velocity, unit, mpc320_velocity",
+    "mpc320_velocity, angular_velocity",
     [
-        (200, pnpq_ureg("degree / second"), 50),
-        (300, pnpq_ureg("degree / second"), 75),
-        (200 * (pi / 180), pnpq_ureg("radian / second"), 50),
-        (300 * (pi / 180), pnpq_ureg("radian / second"), 75),
-        (200 / (170 / 1370), pnpq_ureg("mpc320_step / second"), 50),
-        (300 / (170 / 1370), pnpq_ureg("mpc320_step / second"), 75),
+        (50, 200 * pnpq_ureg("degree / second")),
+        (75, 300 * pnpq_ureg("degree / second")),
+        (50, 3.49065850399 * pnpq_ureg("radian / second")),
+        (75, 5.23598775598 * pnpq_ureg("radian / second")),
+        (50, 1611.76470588 * pnpq_ureg("mpc320_step / second")),
+        (75, 2417.64705882 * pnpq_ureg("mpc320_step / second")),
     ],
 )
 def test_from_mpc320_velocity_conversion(
-    angular_velocity: float, unit: Quantity, mpc320_velocity: float
+    mpc320_velocity: float, angular_velocity: Quantity
 ) -> None:
 
     proportion = mpc320_velocity * pnpq_ureg.mpc320_velocity
-    velocity = proportion.to(unit)
-    assert angular_velocity == pytest.approx(velocity.magnitude)
-    assert unit.units == velocity.units
+    velocity = proportion.to(angular_velocity.units)
+    assert angular_velocity.magnitude == pytest.approx(velocity.magnitude)
+    assert angular_velocity.units == velocity.units
 
 
-def test_to_mpc320_velocity_out_of_bounds() -> None:
-
+@pytest.mark.parametrize(
+    "velocity",
+    [
+        5 * (pnpq_ureg.degree / pnpq_ureg.second),  # Too low
+        450 * (pnpq_ureg.degree / pnpq_ureg.second),  # Too high
+    ],
+)
+def test_to_mpc320_velocity_out_of_bounds(velocity: Quantity) -> None:
     with pytest.raises(ValueError, match="Rounded mpc320_velocity .* is out of range"):
-        velocity = 5 * (pnpq_ureg.degree / pnpq_ureg.second)  # Too low
-        velocity.to(pnpq_ureg.mpc320_velocity)
-
-    with pytest.raises(ValueError, match="Rounded mpc320_velocity .* is out of range"):
-        velocity = 450 * (pnpq_ureg.degree / pnpq_ureg.second)  # Too high
         velocity.to(pnpq_ureg.mpc320_velocity)
